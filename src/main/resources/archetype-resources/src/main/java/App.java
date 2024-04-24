e93e0cd214d3c9f27743283deab0019eb0811ec0
yami201 <mohamednj2013@gmail.com>
2024-04-24 11:08:08 +0100

    test rebase

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Github TP");
         System.out.println("Code written by Dev 2 ");
+        System.out.println("Code written by Dev 2 to rebase ");
     }
 }
