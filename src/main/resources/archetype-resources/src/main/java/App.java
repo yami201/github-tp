90a768ff719557217567fb731a3a70d946aad560
Yasmine1800 <yasminelafhim@gmail.com>
2024-04-24 11:35:59 +0100

    Amelioration du graph de visualisation7

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -10,11 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Github TP");
-        System.out.println("Code written by Dev 2 ");
-        System.out.println("Code written by naabi");
-        System.out.println("Code written by naabi to rebase");
-        System.out.println("--------------------------------");
-        System.out.println("****************************");
        
     }
 }
