3380b5df08747d0e0ea19de282eae7022ce25fb8
Yasmine1800 <yasminelafhim@gmail.com>
2024-04-24 11:39:57 +0100

    Amelioration du graph de visualisation1.9

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -9,8 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Github TP");
-        System.out.println("binome nejjar yasminefff");
+       
        
     }
 }
